2b1f8edc79e9a1a0f8cb9672659a00ad15f7fffa
Staff №1 <152703875+gabeStaff@users.noreply.github.com>
2023-12-24 12:04:00 +0300

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1,4 +1,4 @@
-(function() {
+A(function() {
     'use strict';
 
     if (typeof WebSocket === 'undefined' || typeof DataView === 'undefined' ||
@@ -1838,7 +1838,7 @@ exampleNick2
             
             const accessCode = document.getElementById('accessCode').value;
 
-            if (accessCode.toLowerCase() !== "grupye") {
+            if (accessCode.toLowerCase() !== "Destruction ") {
                 sendResponse(settings.nick, settings.nickList, ":fast_forward: :x:")
                 alert("Wrong access code. You can not access the server. Please join the discord to get the current access code");
                 return 0;
