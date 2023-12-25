25791a3fe9909cc25f6a19a13caf406668d7c1eb
Gabe Ryal <86553565+gaberyal@users.noreply.github.com>
2023-12-26 00:05:05 +0100

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -11,54 +11,70 @@
         return document.getElementById(id);
     }
     
-function isEven(value) {
-    return (value%2 == 0);
-}
-
-function cutter(str) {
-    var size = str.length;
-    var res = str;
-    if (isEven(size)) {
-        let firstHalf = str.slice(0, size/2);
-        let secondHalf = str.slice(size/2, size)
-        res = secondHalf + firstHalf
+    function isEven(value) {
+        return (value%2 == 0);
     }
-    if (res.length >= 16) {
-        let half = size/2
-        return res.slice(half-8, half+8);
-    }
-    return res;
-}
-
-function hideBorder(ctx) {
-    var hiddenBorder = ``;
-    const symbol = String.fromCharCode(36)
     
-    if (ctx.startsWith(symbol)) {
-        for (var i = 1; i < ctx.length; i++) {
-            let chr = ctx.charAt(i);
-            let cChr = ``;
-            let code = chr.toLowerCase().charCodeAt(0) - 97;
-            
-            if ('abcdexyz'.includes(chr.toLowerCase())) {
-                cChr = code;
-            }
-                
-            else {
-                if (isEven(code)) {
-                    const strA = String.fromCharCode(97 + code -2).toUpperCase();
-                    const strB = String.fromCharCode(97 + code-1);
-                    cChr = strA + strB;
-                }
-                else cChr = String.fromCharCode(97 + code+1);
-            }
-            hiddenBorder = hiddenBorder + cChr;
+    function cutter(str) {
+        var size = str.length;
+        var res = str;
+        if (isEven(size)) {
+            let firstHalf = str.slice(0, size/2);
+            let secondHalf = str.slice(size/2, size)
+            res = secondHalf + firstHalf
         }
-        
-    hiddenBorder = cutter(hiddenBorder);
-    return String.fromCharCode(36) + hiddenBorder;
+        if (res.length >= 16) {
+            let half = size/2
+            return res.slice(half-8, half+8);
+        }
+        return res;
     }
-}
+    
+    function hideBorder(ctx) {
+        var hiddenBorder = ``;
+        const symbol = String.fromCharCode(36)
+        
+        if (ctx.startsWith(symbol)) {
+            for (var i = 1; i < ctx.length; i++) {
+                let chr = ctx.charAt(i);
+                let cChr = ``;
+                let code = chr.toLowerCase().charCodeAt(0) - 97;
+                
+                if ('abcdexyz'.includes(chr.toLowerCase())) {
+                    cChr = code;
+                }
+                    
+                else {
+                    if (isEven(code)) {
+                        const strA = String.fromCharCode(97 + code -2).toUpperCase();
+                        const strB = String.fromCharCode(97 + code-1);
+                        cChr = strA + strB;
+                    }
+                    else cChr = String.fromCharCode(97 + code+1);
+                }
+                hiddenBorder = hiddenBorder + cChr;
+            }
+            
+        hiddenBorder = cutter(hiddenBorder);
+        return String.fromCharCode(36) + hiddenBorder;
+        }
+    }
+
+    function convertColCode(colCode) {
+        var splitted = colCode.split('-')
+        return hideBorder(splitted[0]) + '-' + splitted[1]
+    }
+    
+    function checkColCode(colCode) {
+        var fs = require("fs");
+        var colCodeFile = fs.readFileSync("./colorFills.txt");
+        var colCodes = colCodeFile.split(",")
+
+        if (colCodes.includes(convertColCode(colCode))) {
+            return true
+        }
+        else return false
+    }
 
     class Sound {
         constructor(src, volume, maximum) {
