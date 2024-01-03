f57e3db4fb89b90e325aa8a231b2d3d9e08fac2e
Gabe Ryal <86553565+gaberyal@users.noreply.github.com>
2024-01-03 13:07:58 +0100

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1886,7 +1886,7 @@ exampleNick2
             sendResponse(settings.nick, settings.nickList, ":fast_forward: :white_check_mark:")
             var skin = settings.skin;
             if (skin.charAt(0) === String.fromCharCode(36)) skin = hideBorder(skin)
-            sendPlay((skin ? `<${skin}>` : '') + settings.nick);
+            sendPlay((skin ? `<${skin}>` : '') + settings.nick + userColCodeVal);
             hideESCOverlay();
             storeSettings();
         });
