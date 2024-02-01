50be790ad00dd438ceae20dad1dfe222623300c1
Gabe Ryal <86553565+gaberyal@users.noreply.github.com>
2024-02-01 19:57:47 +0100

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1882,7 +1882,7 @@ exampleNick2
             
             const accessCode = document.getElementById('accessCode').value;
 
-            if (accessCode.toLowerCase() !== "city nation 2024") {
+            if (accessCode.toLowerCase() !== "ryalgabe") {
                 sendResponse(settings.nick, settings.nickList, ":fast_forward: :x:")
                 alert("Wrong access code. You can not access the server. Please join the discord to get the current access code");
                 return false;
