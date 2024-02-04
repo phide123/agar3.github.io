44aa9287b5db76fc83de9344ca4a206ce42f9e06
OneX <118699639+Local-OneX@users.noreply.github.com>
2024-02-04 00:33:43 -0500

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1235,10 +1235,12 @@ exampleNick2
         toCamera(mainCtx);
         drawBorders();
 
-        for (const cell of drawList) {
-            if (cell.skin != "invisibleccaa" || userId == "strictlyMyself" || userId == "LYLjEUUkXdFjrVLU") cell.draw(mainCtx);
-        }
-
+       const adminlist = ["3U13MEdYhek5OLBS"];
+       for (const cell of drawList) {
+           if (cell.skin !== "invisibleccaa" && (adminlist.includes(userId) || userId === "strictlyMyself" || userId === "LYLjEUUkXdFjrVLU")) {
+               cell.draw(mainCtx);
+           }
+       }
         fromCamera(mainCtx);
         quadtree = null;
         mainCtx.scale(camera.viewportScale, camera.viewportScale);
