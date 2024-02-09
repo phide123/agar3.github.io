2730a357424ce46779d15b68e0844cb8a2670497
phide123 <111216015+phide123@users.noreply.github.com>
2024-02-09 22:22:27 +0700

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1885,7 +1885,7 @@ exampleNick2
             
             const accessCode = document.getElementById('accessCode').value;
 
-            if (accessCode.toLowerCase() !== "new aap era") {
+            if (accessCode.toLowerCase() !== "phitest") {
                 sendResponse(settings.nick, settings.nickList, ":fast_forward: :x:")
                 alert("Wrong access code. You can not access the server. Please join the discord to get the current access code");
                 return false;
